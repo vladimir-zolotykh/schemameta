aa2b5c980f191bbe227478f8052933d18b837984
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-04-19 23:53:05 +0300

    Refactor TupleMeta
    +        fields = getattr(cls, "_fields", [])
    +        for n, field in enumerate(fields):

--- poly.py
+++ poly.py
@@ -7,21 +7,21 @@ from operator import itemgetter
 class TupleMeta(type):
     def __init__(cls, name, bases, namespace, **kwargs):
         super().__init__(name, bases, namespace, **kwargs)
-        for n, field in enumerate(cls._fields):
+        fields = getattr(cls, "_fields", [])
+        for n, field in enumerate(fields):
             setattr(cls, field, itemgetter(n))
 
 
-class _Tuple(tuple):
+class _Tuple(tuple, metaclass=TupleMeta):
     def __new__(cls, *args):
         assert len(args) == len(cls._fields)
-        print(f"{cls = }")
         return super().__new__(cls, args)
 
     def __repr__(self):
         return f"{self.__class__.__name__}{super().__repr__()}"
 
 
-class Point(_Tuple, metaclass=TupleMeta):
+class Point(_Tuple):
     _fields = ["x", "y"]
 
 
